613384a3bc919e820382476f807e235ebdeff9f7
Jonathan Greene <jon@greenethumb.com>
2015-09-04 16:02:52 -0500

    added node scripts... because why not?!

---
+++ index.js
@@ -0,0 +1,36 @@
+var parser = require('xml2json');
+var request = require('request');
+var fs = require('fs');
+var useRemote = true;
+var xml = "";
+
+if(useRemote){
+  console.log('request XML remotely...');
+  request('https://raw.githubusercontent.com/seth-k/BJCP-styles-XML/master/styleguide-2015.xml', function (err, response, body) {
+    if (err) {
+      return console.log(err);
+    }
+    parseXML(body);
+  })
+} else {
+  console.log('using local XML');
+  fs.readFile('./xml/styleguide-2015.xml', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    parseXML(data);
+  });
+}
+
+function parseXML(xml){
+  var json = parser.toJson(xml);
+  var path = './json/styleguide-2015.min.json';
+
+  fs.writeFile(path, json, function(error) {
+     if (error) {
+       console.error("write error:  " + error.message);
+     } else {
+       console.log("Successful Write to " + path);
+     }
+  });
+}
