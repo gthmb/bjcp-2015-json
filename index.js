80ab48fbdcb2b41c9abed2defb1950848e2b0176
Jon Greene <jon.greene@corvisa.com>
2016-04-16 10:08:53 -0400

    * added non-minified and ES6-module formatted output. * tweaked formatting tags and examples * fixed new-line merging of words

--- index.js
+++ index.js
@@ -4,50 +4,100 @@ var request = require('request');
 var fs = require('fs');
 var useRemote = false;
 var xml = "";
-var dest = "./json/styleguide-2015.min.json";
+var dest = "./json/styleguide-2015";
 
 process.argv.forEach(function(val, index) {
-  console.log('value:', val)
+    if (val === "useRemote=true") {
+        useRemote = true;
+        return;
+    }
 
-  if(val === "useRemote=true"){
-    useRemote = true;
-    return;
-  }
-
-  console.log(val.substr(0,5));
-
-  if(val.substr(0,5) === 'dest='){
-    dest = val.substr(5);
-    return;
-  }
+    if (val.substr(0, 5) === 'dest=') {
+        dest = val.substr(5);
+        return;
+    }
 });
 
-if(useRemote){
-  console.log('request XML remotely...');
-  request('https://raw.githubusercontent.com/seth-k/BJCP-styles-XML/master/styleguide-2015.xml', function (err, response, body) {
-    if (err) {
-      return console.log(err);
-    }
-    parseXML(body);
-  })
+if (useRemote) {
+    console.log('request XML remotely...');
+    request('https://raw.githubusercontent.com/seth-k/BJCP-styles-XML/master/styleguide-2015.xml', function(err, response, body) {
+        if (err) {
+            return console.log(err);
+        }
+        parseXML(body);
+    })
 } else {
-  console.log('using local XML');
-  fs.readFile('./xml/styleguide-2015.xml', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    parseXML(data);
-  });
+    console.log('using local XML');
+    fs.readFile('./xml/styleguide-2015.xml', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        parseXML(data);
+    });
 }
 
-function parseXML(xml){
-  var json = parser.toJson(xml.replace(/(<em>)|(<\/em>)|(\s{2,})/g, ""));
+function parseXML(xml) {
+    var json = JSON.parse(
+        parser.toJson(
+            xml.replace(/(<em>)|(<\/em>)|(\s{2,})|(<strong>)|(<\/strong>)/g, ""), { sanitize: false }
+        )
+    );
 
-  fs.writeFile(dest, json, function(error) {
-     if (error) {
-       console.error("write error:  " + error.message);
-     } else {
-       console.log("Successful Write to " + dest);
-     }
-  });
+    maniuplateDefs(json, 'tags', function(val) {
+        return val.split(/,\s/g);
+    });
+
+    maniuplateDefs(json, 'examples', function(val) {
+
+        if(val.indexOf(';') !== -1){
+            var newVal = {},
+                types = val.split(';');
+
+            for(var el in types){
+                var typename = types[el].substr(0, types[el].indexOf('-')).trim(),
+                    typeval = types[el].substr(types[el].indexOf('-')).replace(/-/, "").trim();
+                newVal[typename] = typeval.split(/,\s/g);
+            }
+            return newVal;
+        }
+
+        return val.split(/,\s/g);
+    });
+
+    var dests = [{
+        fname: dest + '.json',
+        content: JSON.stringify(json, null, 4)
+    }, {
+        fname: dest + '.min.json',
+        content: JSON.stringify(json)
+    },{
+        fname: dest + '.js',
+        content: "export const STYLES = " + JSON.stringify(json)
+    }];
+
+    dests.forEach(function(fobj) {
+        fs.writeFile(fobj.fname, fobj.content, function(error) {
+            if (error) {
+                console.error("write error:  " + error.message);
+            } else {
+                console.log("Successful Write to " + fobj.fname);
+            }
+        });
+    });
+}
+
+function maniuplateDefs(obj, pname, handler) {
+    search = function(o) {
+        if (o.hasOwnProperty(pname)) {
+            o[pname] = handler(o[pname]);
+        }
+
+        for (var el in o) {
+            if (typeof o[el] === 'object' && el !== pname) {
+                search(o[el]);
+            }
+        }
+    }
+
+    search(obj);
 }
